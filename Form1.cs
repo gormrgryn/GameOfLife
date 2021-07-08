4cfef70129ad4a0f145a08cd578046cb58f24b83
gormrgryn <gor-margaryan-05@mail.ru>
2021-07-08 20:31:56 +0400

    first commit, working proect

---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private int resolution;
+        private GameEngine gameEngine;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void StartGame()
+        {
+            if (timer1.Enabled) return;
+
+            nudResolution.Enabled = false;
+            nudDensity.Enabled = false;
+            resolution = (int) nudResolution.Value;
+
+            gameEngine = new GameEngine(
+                pictureBox1.Height / resolution,
+                pictureBox1.Width / resolution,
+                (int)nudDensity.Value
+            );
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            timer1.Start();
+        }
+        void StopGame()
+        {
+            if (!timer1.Enabled) return;
+            nudResolution.Enabled = true;
+            nudDensity.Enabled = true;
+            timer1.Stop();
+        }
+        
+        private void UpdateGen()
+        {
+            graphics.Clear(Color.Black);
+            var field = gameEngine.GetCurrentGen();
+            for (int x = 0; x < field.GetLength(0); x++)
+            {
+                for (int y = 0; y < field.GetLength(1); y++)
+                {
+                    if (field[x, y]) graphics.FillRectangle(Brushes.Coral, x * resolution, y * resolution, resolution, resolution);
+                }
+            }
+            pictureBox1.Refresh();
+            gameEngine.NextGen();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateGen();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            StopGame();
+        }
+    }
+}
---
+++ GameEngine.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class GameEngine
+    {
+        private bool[,] field;
+        private readonly int rows;
+        private readonly int cols;
+        public uint currentGen { get; private set; }
+        private Random random = new Random();
+        public GameEngine(int rows, int cols, int density)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            field = new bool[cols, rows];
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = (random.Next(density) == 0);
+                }
+            }
+        }
+        private int CountNeighbours(int x, int y)
+        {
+            int count = 0;
+            bool[] ngbs = new bool[8]
+            {
+                field[(x+cols) % cols, (y+1+rows) % rows],
+                field[(x+cols) % cols, (y-1+rows) % rows],
+                field[(x+1+cols) % cols, (y+rows) % rows],
+                field[(x-1+cols) % cols, (y+rows) % rows],
+                field[(x+1+cols) % cols, (y+1+rows) % rows],
+                field[(x-1+cols) % cols, (y-1+rows) % rows],
+                field[(x+1+cols) % cols, (y-1+rows) % rows],
+                field[(x-1+cols) % cols, (y+1+rows) % rows]
+            };
+            foreach (bool i in ngbs)
+            {
+                if (i) count++;
+            }
+            return count;
+        }
+        public void NextGen()
+        {
+            var newField = new bool[cols, rows];
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    int neighboursCount = CountNeighbours(x, y);
+                    bool hasLife = field[x, y];
+
+                    if (!hasLife && neighboursCount == 3) newField[x, y] = true;
+                    else if (hasLife && (neighboursCount < 2 || neighboursCount > 3)) newField[x, y] = false;
+                    else newField[x, y] = field[x, y];
+                }
+            }
+            field = newField;
+            currentGen++;
+        }
+        public bool[,] GetCurrentGen()
+        {
+            var res = new bool[cols, rows];
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    res[x, y] = field[x, y];
+                }
+            }
+            return res;
+        }
+    }
+}
